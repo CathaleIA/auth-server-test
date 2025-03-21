bffb6e932aa409ab9c8b1a629ba8e38787fe7155
cathaleia <eficiencia.desarrollo@copowe.com.co>
2025-03-21 09:41:23 -0500

    agregando yml modificado

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  AMPLIFY_APP_ORIGIN: 'https://main.dym1w6ciej86r.amplifyapp.com',
 };
 
 export default nextConfig;
